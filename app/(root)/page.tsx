c98c846483a28b9cf5dadb0b9f4370b0260fb795
Ayush Singh Kushwaha <50234003+Ayush909@users.noreply.github.com>
2024-11-27 01:12:47 +0530

    removing console and comments

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -12,21 +12,6 @@ export default async function Home({
 
   const posts = await client.fetch(STARTUPS_QUERY);
 
-  console.log(JSON.stringify(posts, null, 2));
-
-  // const posts = [
-  //   {
-  //     _createdAt: new Date(),
-  //     views: 34,
-  //     author: { _id: 1, name: "Ayush Singh" },
-  //     _id: 1,
-  //     description: "This is a descr text",
-  //     image:
-  //       "https://images.unsplash.com/photo-1593376853899-fbb47a057fa0?q=80&w=1915&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-  //     category: "Robots",
-  //     title: "We Robots",
-  //   },
-  // ];
   return (
     <>
       <section className="pink_container">
